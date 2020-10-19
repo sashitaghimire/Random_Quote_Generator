b07c0539e03b00d1e9d8079ecdeaebf1d287b970
sashitaghimire <ghimiresashita@gmail.com>
2020-10-19 15:37:21 +0545

    css added

--- src/Quotes.js
+++ src/Quotes.js
@@ -1,10 +1,14 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState, useEffect, useRef} from 'react'
 import './Quotes.css';
 import {Button} from "@material-ui/core";
+import quote from './image/quote.png';
 
 
 function Quotes() {
     const[quotes, setQuotes] = useState("");
+    const textRef = useRef();
+
+    let colors = ["#336d88","#024249", "4d3e3e", "#45046a","#393e46", "#512b58","#552244"];
 
     const getRandomQuote = () =>{
       fetch("https://type.fit/api/quotes").then((res)=>res.json()).then((data)=>{
@@ -18,23 +22,29 @@ function Quotes() {
     useEffect(()=>{
         getRandomQuote();
     },[]);
+
+    useEffect(()=>{
+        textRef.current.style.color = colors[Math.floor(Math.random()*colors.length)];
+    },[quotes]);
     return (
-        <div className="quotes">
-            <div className="quote">
-            <p>{quotes.text}</p>
-
-            </div>
-            <div className="quote__author">
-           <p> {quotes.author}</p>
-
-            </div>
+        <div className="quote">
+            <div className="quotes">
+                <img src={quote} />
+            <p ref={textRef}> "{quotes.text}"</p>
+            <p style={{fontWeight:400}}> Author: {quotes.author}</p>
+            
            
-             <Button onClick={getRandomQuote}>
+            
+           
+           <div className="quotes__button">
+           <Button  onClick={getRandomQuote}>
         Get Quote
       </Button>
-      <a href="#">
-          <Button>Tweet</Button>
+      <a href={`https://twitter.com/intent/tweet?text=${quotes.text}`} target="__blank" rel="noopener noreferrer" className="quotes__link">
+          <Button >Tweet</Button>
       </a>
+           </div>
+        </div>
         </div>
     )
 }
